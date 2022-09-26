a63cf68352cd245414a3932245ae1af10cb0c897
Théodore <theodoreyip19@gmail.com>
2022-09-26 21:28:00 +0300

    handle csv data

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,20 @@ module.exports = {
     title: `Bible Map Project`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    // 'gatsby-transformer-geojson',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'data',
+        path: `${__dirname}/data`,
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-csv',
+      options: {
+        delimiter: 'auto',
+      },
+    },
+  ],
 }
